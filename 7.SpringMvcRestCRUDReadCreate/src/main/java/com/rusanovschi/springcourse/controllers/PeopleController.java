07b469ddf96adfe8a71c6aa8712a69d547feab1d
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-04-05 17:31:19 +0300

    add spring mvc project with REST and CRUD, detailed about create

---
+++ 7.SpringMvcRestCRUDReadCreate/src/main/java/com/rusanovschi/springcourse/controllers/PeopleController.java
@@ -0,0 +1,50 @@
+package com.rusanovschi.springcourse.controllers;
+
+import com.rusanovschi.springcourse.dao.PersonDAO;
+import com.rusanovschi.springcourse.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+
+        model.addAttribute("people", personDAO.index());
+        return "/people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+
+        //System.out.println(personDAO.show(id).getId() + " " + personDAO.show(id).getName());
+
+        model.addAttribute("person", personDAO.show(id));
+        return "/people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+
+        model.addAttribute("person" , new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+}
