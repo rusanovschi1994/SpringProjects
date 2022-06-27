5de35d3e223adcb892554c1803898faba5a81135
rusanovschi1994 <rusanovschi1994@gmail.com>
2022-06-28 02:55:20 +0300

    change PersonDAOImpl with replace 'Session' with 'Entity Manager'

--- 17. SpringBootProject/src/main/java/com/rusanovschi/spring/springboot/dao/PersonDAOImpl.java
+++ 17. SpringBootProject/src/main/java/com/rusanovschi/spring/springboot/dao/PersonDAOImpl.java
@@ -2,11 +2,11 @@ package com.rusanovschi.spring.springboot.dao;
 
 
 import com.rusanovschi.spring.springboot.entity.Person;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 /**
@@ -27,8 +27,10 @@ public class PersonDAOImpl implements PersonDAO{
     @Override
     public List<Person> getAllPeople() {
 
-        Session session = entityManager.unwrap(Session.class);
-        List<Person> allPerson = session.createQuery("from Person ").getResultList();
+//        Session session = entityManager.unwrap(Session.class);
+//        List<Person> allPerson = session.createQuery("from Person ").getResultList();
+        Query query = entityManager.createQuery("from Person");
+        List<Person> allPerson = query.getResultList();
 
         return allPerson;
     }
@@ -36,58 +38,39 @@ public class PersonDAOImpl implements PersonDAO{
     @Override
     public void savePerson(Person person) {
 
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(person);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(person);
+        Person newPerson =  entityManager.merge(person);
+        person.setId(newPerson.getId());
     }
 
     @Override
     public Person getPerson(int id) {
 
-        Session session = entityManager.unwrap(Session.class);
-        Person person = session.get(Person.class, id);
+//        Session session = entityManager.unwrap(Session.class);
+//        Person person = session.get(Person.class, id);
+        Person person = entityManager.find(Person.class, id);
         return person;
     }
 
     @Override
     public void updatePerson(int id, Person updatedPerson) {
 
-        Session session = entityManager.unwrap(Session.class);
-        Person personTobeUpdated = session.get(Person.class, id);
+//        Session session = entityManager.unwrap(Session.class);
+//        Person personTobeUpdated = session.get(Person.class, id);
+        Person personToBeUpdated = entityManager.find(Person.class, id);
 
-        personTobeUpdated.setName(updatedPerson.getName());
-        personTobeUpdated.setAge(updatedPerson.getAge());
-        personTobeUpdated.setEmail(updatedPerson.getEmail());
-        personTobeUpdated.setPhone(updatedPerson.getPhone());
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        personToBeUpdated.setPhone(updatedPerson.getPhone());
     }
 
     @Override
     public void deletePerson(int id) {
 
-        Session session = entityManager.unwrap(Session.class);
-        session.remove(session.get(Person.class, id));
+//        Session session = entityManager.unwrap(Session.class);
+//        session.remove(session.get(Person.class, id));
+        entityManager.remove(entityManager.find(Person.class, id));
     }
-
-//    public Person show(int id) {
-//
-//       return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?", new Object[]{id}, new PersonMapper())
-//               .stream().findAny().orElse(null);
-//    }
-//
-//    public void save(Person person) {
-//
-//        jdbcTemplate.update("INSERT INTO Person VALUES (default , ?, ?, ?, ?)", person.getName(), person.getAge(), person.getEmail(), person.getPhone());
-//    }
-//
-//    public void update(int id, Person updatedPerson) {
-//
-//        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, phone=? WHERE id=?",updatedPerson.getName(),
-//                updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getPhone(), id);
-//    }
-//
-//    public void delete(int id) {
-//
-//        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
-//    }
-
-
 }
