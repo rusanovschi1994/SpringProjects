8b017a6dcfd95b97449c6ddfb8663e567cf7cd2c
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-08-02 15:25:28 +0300

    change CustomerRepository/CustomerService

--- shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/repository/CustomerRepository.java
+++ shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import com.rusanovschi.shop.beautifulnails.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
