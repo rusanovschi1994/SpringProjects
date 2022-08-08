715b0ab2937bf68aa3c7abbe0e2d41042b1e9a97
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-08-08 09:19:37 +0300

    delete AuthProviderImpl and use OutUserDetailsService in SecurityConfig

--- shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/config/SecurityConfig.java
+++ shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package com.rusanovschi.shop.beautifulnails.config;
 
-import com.rusanovschi.shop.beautifulnails.security.AuthProviderImpl;
+import com.rusanovschi.shop.beautifulnails.service.OurUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final OurUserDetailsService ourUserDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(OurUserDetailsService ourUserDetailsService) {
+        this.ourUserDetailsService = ourUserDetailsService;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(ourUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+
+        return NoOpPasswordEncoder.getInstance();
     }
 }
