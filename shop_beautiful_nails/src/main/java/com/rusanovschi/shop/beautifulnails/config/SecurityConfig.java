be73d65cff3854d0eea0744996db264bf34b47ab
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-08-09 11:18:52 +0300

    added new AuthController and loginPage

--- shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/config/SecurityConfig.java
+++ shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.rusanovschi.shop.beautifulnails.service.OurUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,9 +20,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.ourUserDetailsService = ourUserDetailsService;
     }
 
+    //Method for authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(ourUserDetailsService);
+    }
+
+    //Method for authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/auth/login", "/error").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/auth/login")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/auth/login?error")
+                    .and()
+                .logout()
+                    .permitAll();
     }
 
     @Bean
