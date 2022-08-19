61548e33597f6b45889e552fa33f4dbbafaedc09
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-08-19 17:36:08 +0300

    added JsonNode

--- RestAPIConsumer/src/main/java/provectapos/UserLogin.java
+++ RestAPIConsumer/src/main/java/provectapos/UserLogin.java
@@ -1,15 +1,22 @@
 package provectapos;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import provectapos.measure.Piece;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class UserLogin {
 
-    public static void main(String[] args) {
+    private static Piece Piece;
+
+    public static void main(String[] args) throws JsonProcessingException {
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -23,30 +30,35 @@ public class UserLogin {
         Login response = restTemplate.postForObject(url, request, Login.class);
         System.out.println(response);
 
-//        //getMeasure
-//        String getMeasureUrl = "http://zamolxis.cloudapp.net:60034/api/v1/stock/measure/select";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.add("Authorization", "Bearer " + response.getToken());
-//
-//
-//        restTemplate.exchange(getMeasureUrl, headers, Piece.class);
 
-        String theUrl = "http://zamolxis.cloudapp.net:60034/api/v1/stock/measure/select";
+        //###############################################################################
+        String theUrl = "http://zamolxis.cloudapp.net:60034/api/v1/stock/article/select";
         RestTemplate restTemplate2 = new RestTemplate();
-        try {
+
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.add("Authorization", "Bearer " + response.getToken());
             HttpEntity<String> entity = new HttpEntity<String>(headers);
             ResponseEntity<String> response2 = restTemplate2.exchange(theUrl, HttpMethod.GET, entity, String.class);
-            System.out.println(response2);
-        }
-        catch (Exception eek) {
-            System.out.println("** Exception: "+ eek.getMessage());
-        }
 
+            String responseEntity = response2.getBody();
+            System.out.println(responseEntity);
 
+            //Parsam JSON-ul primit
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode nodes = mapper.readTree(responseEntity);
+
+             for (JsonNode node: nodes) {
+
+                    StringBuilder builder = new StringBuilder();
+                     builder
+                             .append(node.findValues("Name"))
+                             .append(" - ")
+                             .append(node.findValues("Price"));
+                     System.out.println(builder);
+
+//                 String st = node.findValues("Name").toString();
+//                 System.out.println(st);
+            }
     }
 }
