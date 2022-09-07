777d851d1c835296d72fc10002d24e329f056ba6
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-09-07 15:43:44 +0300

    added dao functionality

--- shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/dto/CustomerDTO.java
+++ shop_beautiful_nails/src/main/java/com/rusanovschi/shop/beautifulnails/dto/CustomerDTO.java
@@ -2,7 +2,10 @@ package com.rusanovschi.shop.beautifulnails.dto;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
@@ -11,21 +14,21 @@ import java.time.LocalDate;
 
 public class CustomerDTO {
 
-    @Column(name = "first_name", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     @NotEmpty(message = "Name should not be empty ")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String firstName;
 
-    @Column(name = "second_name", nullable = false)
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String secondName;
 
-    @Column(name = "phone", nullable = false)
     @NotEmpty(message = "Phone number should not be empty")
     private String phoneNumber;
 
-    @Column(name = "email", unique = true)
     @Email(message = "Email is not valid")
     private String email;
 
@@ -33,13 +36,19 @@ public class CustomerDTO {
 //    @Temporal(TemporalType.DATE)
 //    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
 //    private Date dob;
-    @Column(name = "birth_date")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dob;
 
-    @Column(name = "address")
     @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Address should be in this format: Country, City, Postal Code(6 characters)")
     private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
