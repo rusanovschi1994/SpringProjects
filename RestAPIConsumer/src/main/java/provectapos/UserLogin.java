adffdb9fb152172e8d886300e42311d1af20509a
RUSANOVSCHI <rusanovschi1994@gmail.com>
2022-09-28 17:02:57 +0300

    added new function for Json parsing

--- RestAPIConsumer/src/main/java/provectapos/UserLogin.java
+++ RestAPIConsumer/src/main/java/provectapos/UserLogin.java
@@ -3,9 +3,7 @@ package provectapos;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.ValueNode;
+import org.json.JSONArray;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import provectapos.measure.Piece;
@@ -35,32 +33,47 @@ public class UserLogin {
         String theUrl = "http://zamolxis.cloudapp.net:60034/api/v1/stock/article/select";
         RestTemplate restTemplate2 = new RestTemplate();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.add("Authorization", "Bearer " + response.getToken());
-            HttpEntity<String> entity = new HttpEntity<String>(headers);
-            ResponseEntity<String> response2 = restTemplate2.exchange(theUrl, HttpMethod.GET, entity, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Bearer " + response.getToken());
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        ResponseEntity<String> response2 = restTemplate2.exchange(theUrl, HttpMethod.GET, entity, String.class);
 
-            String responseEntity = response2.getBody();
-            System.out.println(responseEntity);
+        String responseEntity = response2.getBody();
+        System.out.println(responseEntity);
 
-            //Parsam JSON-ul primit
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode nodes = mapper.readTree(responseEntity);
+        //Parse Json to String
+        String json = responseEntity;
+        JSONArray arr = new JSONArray(json);
+        for (int i = 0; i < arr.length(); i++){
 
-             for (JsonNode node: nodes) {
+            String articleMeasureType = arr.getJSONObject(i).getString("ArticleMeasureType");
 
-                    StringBuilder builder = new StringBuilder();
-                     builder
-                             .append(node.findValues("Name"))
-                             .append(" - ")
-                             .append(node.findValues("Price"))
-                             .append(" - ")
-                             .append(node.findValues("ArticleMeasureType")) ;
-                     System.out.println(builder);
+            if(articleMeasureType.equals("Weighted")){
+                String name = arr.getJSONObject(i).getString("Name");
+                Integer price = arr.getJSONObject(i).getInt("Price");
+                System.out.println(name + " " + price);
+            }
+            //System.out.println("Name: " + name + ", Price: " + price + ", Type: " + articleMeasureType);
+        }
+
+
+//            //Parsam JSON-ul primit
+//            ObjectMapper mapper = new ObjectMapper();
+//            JsonNode nodes = mapper.readTree(responseEntity);
+//
+//             for (JsonNode node: nodes) {
+//
+//                    StringBuilder builder = new StringBuilder();
+//                     builder
+//                             .append(node.findValues("Name"))
+//                             .append(" - ")
+//                             .append(node.findValues("Price"))
+//                             .append(" - ")
+//                             .append(node.findValues("ArticleMeasureType")) ;
+//                     System.out.println(builder);
 
 //                 String st = node.findValues("Name").toString();
 //                 System.out.println(st);
             }
     }
-}
